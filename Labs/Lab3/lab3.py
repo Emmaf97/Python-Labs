4e7edb9e62edd5d29620e455206d8b721355f9e1
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-02-23 18:23:09 +0000

    Lab3 v1

---
+++ Labs/Lab3/lab3.py
@@ -0,0 +1,8 @@
+# 1. Checking Eligibility
+age = int(input("Enter your age to vote: "))
+if age > 17:
+    print("You are able to vote")
+else:
+    print("You are not able to vote")
+    
+#2. Grading System
