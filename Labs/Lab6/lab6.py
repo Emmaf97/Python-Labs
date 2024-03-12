490da9dc816615afc4849fb43cabd70fc54fd632
Emmaf97 <43826775+Emmaf97@users.noreply.github.com>
2024-03-12 16:59:55 +0000

    Initial upload

---
+++ Labs/Lab6/lab6.py
@@ -0,0 +1,24 @@
+# 1. Celsius to Fahrenheit Conversion
+# Write a Python function that converts a temperature from Celsius to Fahrenheit. The
+# function should take a temperature in Celsius as an argument and return the
+# equivalent temperature in Fahrenheit.
+
+
+#2. Maximum of Three Numbers
+# Write a Python function that takes three numbers as arguments and returns the
+# maximum of the three numbers.
+
+#3. Palindrome Check
+# Write a Python function that checks whether a given string is a palindrome or not.
+# The function should take a string as an argument and return True if it is a
+# palindrome, and False otherwise.
+
+
+#4. Factorial Calculation
+# Write a Python function that calculates the factorial of a given number. The function
+# should take an integer as an argument and return its factorial.
+
+#5. Sum of Squares
+# Write a Python function that calculates the sum of squares of numbers from 1 to a
+# given number. The function should take a positive integer as an argument and return
+# the sum of squares.
